05866f7c31b8b3942182dd3452d05966f41309a2
Ravi <ravi@innasoft.in>
2018-01-04 09:22:10 +0530

    Default paramaters

---
+++ Default_Parameters.js
@@ -0,0 +1,12 @@
+//Reference https://www.youtube.com/watch?v=0_5Nns9YgjQ&feature=em-uploademail
+function add(a ,b){
+	
+	if(a === undefined)
+		a = 0;
+	if(b === undefined)
+		b = 0;
+	
+	return a + b ;
+}
+
+console.log(add(1,2))
