40e840e8a8337d4582babfad6d791c22431a2cb7
RAVI <sr27631.com>
2019-08-19 22:22:44 +0530

    call_apply_bind Explore

--- call_apply_bind.js
+++ call_apply_bind.js
@@ -15,14 +15,23 @@ var fun = function(a){
     return this.result*a
 
 }
-
 console.log(fun.call(obj ,2))
-
-
 console.log("Moretan one arguments call function START")
 var fun1 = function(a ,b){
     return this.result*a*b
-
 }
-
 console.log(fun1.call(obj ,2 ,4))
+
+// APPLY FUNCTION
+arguments =[20]
+console.log(fun.apply(obj ,arguments))
+
+arguments =[20 ,40]
+console.log(fun1.apply(obj ,arguments))
+
+//bind EXAMPLE
+
+var b = fun.bind(obj);
+console.log(b(10))
+console.log(b)
+console.dir(obj)
