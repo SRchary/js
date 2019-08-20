9dfaf9e61730f41149d392fccb289af19ec4b3ce
RAVI <sr27631.com>
2019-08-20 12:23:18 +0530

    call_apply_bind Explore

---
+++ closer.js
@@ -0,0 +1,17 @@
+
+/*
+NOTE : Javascript is Lexical Scoping . it means outer function can`t access inner function data. But Inner Function Can access th outer function Data
+
+* Closer is Nothing but a function with preserve data. it means inner function can access out side  the data even out side function is deleted
+*
+* */
+/* EXAMPLE ONE------------------1 */
+var fun = function(passed_ver){
+    return 2+passed_ver
+}
+console.dir(fun)
+
+/* EXAMPLE with innerfunction ---------------------2 */
+
+
+
