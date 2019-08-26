2a6790a848aaa03ce078a750c08ce49d83d09df8
RAVI <sr27631.com>
2019-08-26 19:51:11 +0530

    call_apply_bind Explore

--- INTERVIEWS/interview_1.js
+++ INTERVIEWS/interview_1.js
@@ -115,6 +115,23 @@ for (let i = 0;i < arr.length ;  i++ ){
 
 /*************************** Question ---> 7  *******************************/
 
+var arry = [1 ,2,2,10 ,1,10,10 ,5];
+// find max value in array and how many present in max value count
+var maxVal = Math.max(...arry);
+console.log(maxVal);
+
+var count =0;
+
+for( var i =0 ; i< arry.length;i++){
+
+    if(maxVal == arry[i]){
+        count++;
+
+    }
+}
+console.log(count)
+
+
 
 
 
